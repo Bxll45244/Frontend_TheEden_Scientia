8e8e5052d3ff1652bba65536b9abba4d5a35dd3e
Bxll45244 <Ccogerbell501@gmail.com>
2025-08-21 07:46:19 +0700

    api

--- src/service/api.js
+++ src/service/api.js
@@ -1,11 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
-    withCredentials: true, // ส่ง HttpOnly Cookie
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? "http://localhost:5000/api",
+  withCredentials: true, 
+  headers: {
+    Accept: "application/json", 
+    "Content-Type": "application/json", 
+  },
 });
 
 export default api;
